b9282eb7e984dad714ac4a6f25e70e624ca0450f
alhanger <hanger129@gmail.com>
2015-10-22 08:02:03 -0400

    Add more functionality

--- app/src/main/java/com/theironyard/contactsandroid/Main2Activity.java
+++ app/src/main/java/com/theironyard/contactsandroid/Main2Activity.java
@@ -2,13 +2,17 @@ package com.theironyard.contactsandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener{
 
     TextView nameBox;
     TextView phoneBox;
+    EditText workPhoneBar;
+    EditText emailBar;
+    EditText addressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +21,21 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         nameBox = (TextView) findViewById(R.id.nameBox);
-        phoneBox = (TextView) findViewById(R.id.phoneBox);
+        //phoneBox = (TextView) findViewById(R.id.phoneBox);
+        workPhoneBar = (EditText) findViewById(R.id.workPhoneBar);
+        emailBar = (EditText) findViewById(R.id.emailBar);
+        addressBar = (EditText) findViewById(R.id.addressBar);
 
         nameBox.setText(content);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String workPhone = workPhoneBar.getText().toString();
+        String email = emailBar.getText().toString();
+        String address = addressBar.getText().toString();
+        workPhoneBar.setText(workPhone);
+        emailBar.setText(email);
+        addressBar.setText(address);
     }
 }
