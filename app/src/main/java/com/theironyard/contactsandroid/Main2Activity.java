aa03ce53a772341b2cf01223d6f9815973cf2808
alhanger <hanger129@gmail.com>
2015-10-21 20:18:37 -0400

    Minor changes to UI

--- app/src/main/java/com/theironyard/contactsandroid/Main2Activity.java
+++ app/src/main/java/com/theironyard/contactsandroid/Main2Activity.java
@@ -2,12 +2,23 @@ package com.theironyard.contactsandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    TextView nameBox;
+    TextView phoneBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String content = getIntent().getStringExtra("content");
         setContentView(R.layout.activity_main2);
+
+        nameBox = (TextView) findViewById(R.id.nameBox);
+        phoneBox = (TextView) findViewById(R.id.phoneBox);
+
+        nameBox.setText(content);
     }
 }
--- app/src/main/java/com/theironyard/contactsandroid/MainActivity.java
+++ app/src/main/java/com/theironyard/contactsandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.theironyard.contactsandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         addButton.setOnClickListener(this);
         listView.setOnItemLongClickListener(this);
+        listView.setOnItemClickListener(this);
 
         contacts = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
         listView.setAdapter(contacts);
@@ -52,6 +54,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("content", contacts.getItem(position));
+        startActivity(intent);
     }
 }
